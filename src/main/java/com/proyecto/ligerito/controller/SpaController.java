74bda06f492aa2f3bd96c0ac0c0bfd799c4600c4
DiegoFL <dieg0s_04@hotmail.com>
2026-05-19 11:17:52 +0200

    pantalla en blanco correccion en spacontroller

--- src/main/java/com/proyecto/ligerito/controller/SpaController.java
+++ src/main/java/com/proyecto/ligerito/controller/SpaController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = { "/{path:[^\\.]*}", "/{path:[^\\.]*}/**" })
+    @RequestMapping(value = {
+        "/{p1:[^\\.]*}",
+        "/{p1:[^\\.]*}/{p2:[^\\.]*}",
+        "/{p1:[^\\.]*}/{p2:[^\\.]*}/{p3:[^\\.]*}"
+    })
     public String forward() {
         return "forward:/index.html";
     }
